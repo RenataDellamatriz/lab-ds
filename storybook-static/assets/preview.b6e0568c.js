e6925ff22910a713f4021c8930c3db39c10c7edb
Renata Dellamatriz <r.dellamatriz@hotmail.com>
2022-10-13 22:20:18 -0300

    using deploy-storybook

---
+++ storybook-static/assets/preview.b6e0568c.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.cabb4e3e.js";import"./iframe.b7da35ab.js";import"./es.map.constructor.01d7f28f.js";import"./es.number.to-fixed.c589544f.js";import"./index.e8602c63.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.b6e0568c.js.map
